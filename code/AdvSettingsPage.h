424a9fe57d9705fbc022a075300def2e7a37725a
Iss Mneur <iss.mneur@telus.net>
2009-10-21 00:25:51 -0600

    The tabs apear on the left side, and they have have windows in them. Everything compiles.

---
+++ code/AdvSettingsPage.h
@@ -0,0 +1,11 @@
+#ifndef ADVSETTINGSPAGE_H
+#define ADVSETTINGSPAGE_H
+
+#include <wx/wx.h>
+
+class AdvSettingsPage: public wxWindow {
+public:
+	AdvSettingsPage(wxWindow* parent);
+};
+
+#endif
---
+++ code/ModsPage.h
@@ -0,0 +1,11 @@
+#ifndef MODSPAGE_H
+#define MODSPAGE_H
+
+#include <wx/wx.h>
+
+class ModsPage: public wxWindow {
+public:
+	ModsPage(wxWindow* parent);
+};
+
+#endif
