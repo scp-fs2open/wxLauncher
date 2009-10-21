75b620babf125713e688e5ee315928aa5b5a10a7
Iss Mneur <iss.mneur@telus.net>
2009-10-20 22:40:17 -0600

    Builds in both Release and Debug mode.

---
+++ code/wxIDS.h
@@ -0,0 +1,8 @@
+#ifndef IDS_HPP
+#define IDS_HPP
+
+/* File contains all IDs that are to be used in the application. */
+
+#define ID_MAINWINDOW 1
+
+#endif
---
+++ code/wxLauncherApp.h
@@ -0,0 +1,9 @@
+#ifndef WXLAUNCHERAPP_H
+#define WXLAUNCHERAPP_H
+#include <wx/wx.h>
+
+class wxLauncher: public wxApp {
+	virtual bool OnInit();
+};
+
+#endif
