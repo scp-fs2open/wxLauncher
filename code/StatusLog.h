64852acd5336f2394ae9b19dfdd5333b60072b42
Iss Mneur <iss.mneur@issmneur.ca>
2009-10-26 00:48:57 -0600

    The JobData class has been fleshed out. StatusTable now has some locks to protect the data. Jobs are now stored in a wxList.

--- code/StatusLog.h
+++ code/StatusLog.h
@@ -9,10 +9,33 @@ enum StatusLogIDS: int {
 	ID_INFO_PANEL,
 };
 
+class StatusLog;
+
 class JobData {
+public:
+	/** Pulses the progress bar for indeterminate jobs. */
+	void Pulse();
+	/** Set the minimum and maximum values that will be passed with SetValue(). */
+	void SetRange(int max, int min=0);
+	/** Set the position in the range set by SetRange(). */
+	void SetValue(int value);
+	/** Set the statusbar message. Method copies the message that is passed in. */
+	void SetMessage(wxString& msg);
+	/** Mark job as done so that it is not included in the job list. */
+	void Done();
+
+private:
+	friend StatusLog;
+	JobData(StatusLog* manager, int max=100, int min=0);
+	StatusLog* myManager;
+	int max, min, value;
+	bool active;	//!< !active jobData's will be reaped on StatusLog's next pass
+	bool beingShown;//!< when true, this job data is the one that is being shown to the user
+	bool pulseState;//!< when true job was last pulsed for its update.
+	wxString* message;
 };
 
-WX_DECLARE_HASH_MAP( int, JobData*, wxIntegerHash, wxIntegerEqual, JobTable);
+WX_DECLARE_LIST( JobData, JobTable);
 
 class StatusLog {
 public:
@@ -21,6 +44,8 @@ public:
 	wxPanel* GetGaugePanel(wxWindow* parent);
 	wxPanel* GetInfoPanel(wxWindow* parent);
 	bool IsOk();
+
+
 
 protected:
 	class StatusLogInfoPanel: public wxPanel {
@@ -38,6 +63,7 @@ protected:
 	protected:
 		void SetMessage(wxString &msg);
 	private:
+		friend JobData;
 		wxString* currentMessage;
 		wxFont *enabledFont, *disabledFont, *hoverFont;
 		wxColour *enabledColour, *disabledColour;
@@ -47,9 +73,14 @@ protected:
 	};
 
 private:
+	friend JobData;
 	wxPanel *gaugePanel;
 	StatusLogInfoPanel *infoPanel;
 	wxGauge *totalProgress, *taskProgress;
+
+	wxCriticalSection *taskProgressLock;	//!< lock held when making changes to the progress bar.
+	wxCriticalSection *jobTableLock;		//!< lock held when making changes to the jobTable.
+	wxCriticalSection *messageLock;			//!< lock held when changing the currentMessage.
 
 	JobTable *jobTable;
 
