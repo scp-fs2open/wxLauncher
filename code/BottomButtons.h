5b70b8fe2b3768a27f1608ff32b6d6157cb80db2
Iss Mneur <iss.mneur@issmneur.ca>
2009-10-25 22:11:23 -0600

    Moved the implementation of the status bar to its own files. It is now rendering correctly in the launcher doesn't do anything.

--- code/BottomButtons.h
+++ code/BottomButtons.h
@@ -3,24 +3,12 @@
 
 #include <wx/wx.h>
 
-class JobData {
-};
-
-WX_DECLARE_HASH_MAP( int, JobData*, wxIntegerHash, wxIntegerEqual, JobTable);
-
 class BottomButtons: public wxPanel {
 public:
 	BottomButtons(wxWindow* parent, wxPoint &pos, wxSize &size);
-
-	void Done();
-	void UpdateBars();
-
 private:
-	wxButton *close, *help, *fred, *log, *update, *play;
-	wxGauge* bar;
-
-	JobTable* joblist;
-	wxFont *disabledFont, *enabledFont;
+	wxButton *close, *help, *fred, *update, *play;
+	wxPanel *gaugePanel, *log;
 
 };
 
