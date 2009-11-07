d4ee4f6cf89de8953fcec006383e35100dd67221
Iss Mneur <iss.mneur@issmneur.ca>
2009-11-07 00:16:47 -0700

    Only 5 leaks left.

--- code/BottomButtons.cpp
+++ code/BottomButtons.cpp
@@ -14,16 +14,9 @@ BottomButtons::BottomButtons(wxWindow* parent, wxPoint &pos, wxSize &size) : wxP
 		this->fred = new wxButton(this, ID_FRED_BUTTON, _("Fred"));
 		this->update = new wxButton(this, ID_UPDATE_BUTTON, _("Update Available"));
 		this->play = new wxButton(this, ID_PLAY_BUTTON, _("Play"));
-		StatusLog* statusLog = new StatusLog();
-		this->log = statusLog->GetInfoPanel(this);
-		this->gaugePanel = statusLog->GetGaugePanel(this);
 		wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-		sizer->Add(this->gaugePanel); /* HACKHACK: hard coded spacer to move the help
-		gaugePanel->SetMinSize(wxSize(150, -1));/* HACKHACK: hard coded spacer to move the help
-							   button from under the tab list. */
 		sizer->Add(this->close);
 		sizer->Add(this->help);
-		sizer->Add(this->log,2);
 		sizer->Add(this->update);
 		sizer->Add(this->fred);
 		sizer->Add(this->play);
