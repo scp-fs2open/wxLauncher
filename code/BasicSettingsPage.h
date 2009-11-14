7c64765beb9ac02fe50c0a96d0582cf1895c3212
Iss Mneur <iss.mneur@issmneur.ca>
2009-11-14 00:08:40 -0700

    Handlers and writing of the selected options for all video options will now work.

--- code/BasicSettingsPage.h
+++ code/BasicSettingsPage.h
@@ -12,7 +12,15 @@ public:
 	void OnTCChanged(wxCommandEvent &event);
 
 	void OnSelectExecutable(wxCommandEvent &event);
-	void OnSelectResolution(wxCommandEvent &event);
+	void OnSelectVideoResolution(wxCommandEvent &event);
+	void OnSelectGraphicsAPI(wxCommandEvent &event);
+	void OnSelectVideoDepth(wxCommandEvent &event);
+	void OnSelectVideoAnistropic(wxCommandEvent &event);
+	void OnSelectVideoAntiAlias(wxCommandEvent &event);
+	void OnSelectVideoTextureFilter(wxCommandEvent &event);
+	void OnSelectVideoGeneralSettings(wxCommandEvent &event);
+	void OnToggleVideoUseLargeTexture(wxCommandEvent &event);
+	void OnToggleVideoFixFontDistortion(wxCommandEvent &event);
 
 private:
 	void FillExecutableDropBox(wxChoice* exeChoice, wxFileName path);
