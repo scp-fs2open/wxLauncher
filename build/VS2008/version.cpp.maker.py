bc97bbc48b9b046661a93bf37d3721401973e1bb
Iss Mneur <iss.mneur@issmneur.ca>
2009-11-19 10:37:21 -0700

    The version info job now updates correctly only when changes to the hg repo happend. The wxLauncher job does not stomp on version info's build files.

--- build/VS2008/version.cpp.maker.py
+++ build/VS2008/version.cpp.maker.py
@@ -9,12 +9,15 @@ def main():
     print "Output file not specifed!"
     sys.exit(1)
     
+  work = os.path.normcase(os.path.normpath(sys.argv[WORKFILE]))
+  file = os.path.normcase(os.path.normpath(sys.argv[OUTFILE]))
+    
   if ( sys.argv[JOB] == "build" ):
-    build(sys.argv[OUTFILE], sys.argv[WORKFILE])
+    build(file, work)
   elif ( sys.argv[JOB] == "rebuild" ):
-    rebuild(sys.argv[OUTFILE], sys.argv[WORKFILE])
+    rebuild(file, work)
   elif ( sys.argv[JOB] == "clean" ):
-    clean(sys.argv[OUTFILE], sys.argv[WORKFILE])
+    clean(file, work)
   else:
     print "Invalid JOB type. Use build, rebuild, or clean."
     sys.exit(2)
@@ -22,22 +25,38 @@ def main():
   sys.exit(0)
 
 def rebuild(file, work):
+  print "Rebuild...."
   clean(file, work)
   build(file, work)
   
 def clean(file, work):
+  print "Clean..."
   try:
+    print " Removing:", file, 
     os.remove(file)
+    print "Done."
   except:
-    pass
+    print "Failed"
     
   try:
+    print " Removing:", work, 
     os.remove(work)
+    print "Done."
   except:
-    pass
+    print "Failed"
   
 def build(file, work):
+  print "Building..."
   if ( need_to_update(file, work) ):
+    print " Writing tempfile:", work
+    make_directory_for_output(work)
+    temp = open(work, "wb")
+    temp.write(get_hg_id())
+    temp.flush()
+    temp.close()
+    
+    print " Writing output file:", file
+    make_directory_for_output(file)
     out = open(file, "wb")
     out.write("wchar_t *HGVersion = L\"")
     out.flush()
@@ -53,6 +72,9 @@ def build(file, work):
     
     out.write('";\n')
     out.close()
+    print " Done"
+  else:
+    print " Up to date"
     
 def get_hg_id():
   id = tempfile.TemporaryFile()
@@ -66,7 +88,28 @@ def need_to_update(file, work):
       workfile = open(work, 'rb')
       if ( workfile.readline() == get_hg_id() ):
         return False
+      else:
+        print "hg id changed"
+    else:
+      print " ", file, "does not exist"
+  else:
+    print " ", work, "does not exist"
   return True
+  
+def make_directory_for_output(output):
+  path = os.path.dirname(output)
+  if os.path.exists(path):
+    if os.path.isdir(path):
+      print " ", path, " already exists"
+    else:
+      print " ", path, " exists but is not a directory!"
+  else:
+    # can only throw an exception user does not have permission
+    try:
+      os.makedirs(path)
+    except:
+      print " ", path, " is not createable"
+      raise
 
 if __name__ == "__main__":
   main()
