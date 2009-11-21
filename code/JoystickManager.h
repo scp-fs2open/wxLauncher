e5002d6e191442b3d1e86c6ce5c318155f9714f8
Iss Mneur <iss.mneur@telus.net>
2009-11-20 22:30:08 -0700

    Added missing JoystickManager.cpp and .h that were supposed to be included in revision d73e0cf5fa10

---
+++ code/JoystickManager.h
@@ -0,0 +1,22 @@
+#ifndef JOYSTICKMANAGER_H
+#define JOYSTICKMANAGER_H
+
+#include <wx/wx.h>
+
+#define JOYMAN_INVAILD_JOYSTICK 99999
+
+namespace JoyMan {
+	bool Initialize();
+	bool DeInitialize();
+	bool IsInitialized();
+	bool WasCompiledIn();
+
+	unsigned int NumberOfJoysticks();
+	bool SupportsForceFeedback(unsigned int i);
+	bool HasCalibrateTool(unsigned int i);
+	void LaunchCalibrateTool(unsigned int i);
+	wxString JoystickName(unsigned int i);
+	bool IsJoystickPluggedIn(unsigned int i);
+};
+
+#endif
