0572bb042c6f0d1a80e64aebde40d7f77e8f003b
Iss Mneur <iss.mneur@telus.net>
2009-11-30 16:45:43 -0700

    Some more fixes for onlinehelpmaker.py

--- scripts/onlinehelpmaker.py
+++ scripts/onlinehelpmaker.py
@@ -136,7 +136,7 @@ def main(argv):
 
     def cleanup(dir):
       if os.path.exists(dir):
-        shutil.rmtree(options.temp, ignore_errors=True)
+        shutil.rmtree(options.temp, onerror=rmtree_error_handler)
 
     atexit.register(cleanup, options.temp)
 
@@ -163,6 +163,8 @@ def call_logging_exceptions(func, *args, **kwargs):
     for filename, line, function, text in traceback.extract_tb(tb):
       logging.error("%s:%d %s", os.path.basename(filename), line, function)
       logging.error("   %s", text)
+      
+    del tb
     
 def rebuild(options):
   logging.log(NOTICE, "Rebuilding")
@@ -184,13 +186,26 @@ def clean(options):
   logging.info("Removing workdirectory: %s", options.temp )
   if os.path.exists(options.temp):
     if os.path.isdir(options.temp):
-      shutil.rmtree(options.temp)
+      shutil.rmtree(options.temp, onerror=rmtree_error_handler)
     else:
       logging.error("tempdir is not a file. Make sure your parameters are in the correct order")
       sys.exit(2)
   else:
     logging.info(" Work directory (%s) does not exist", options.temp)
 
+def rmtree_error_handler(function, path, excinfo):
+  if function == os.remove:
+    logging.warning("  Unable to remove %s", path)
+  elif function == os.rmdir:
+    logging.warning("  Unable to remove directory %s", path)
+  else:
+    (type, value, tb) = excinfo
+    logging.error("***EXCEPTION:")
+    logging.error(" %s: %s", type.__name__, value)
+    for filename, line, function, text in traceback.extract_tb(tb):
+      logging.error("%s:%d %s", os.path.basename(filename), line, function)
+      logging.error("   %s", text)
+    
 
 def build(options):
   """Compiles the files in options.indir to the archive output options.outfile.
