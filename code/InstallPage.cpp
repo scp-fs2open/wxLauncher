900885c24f6b80893a002b560c9f1bcacf89db71
killermosi <unknown>
2010-01-17 23:31:58 +0200

    Tweaks on the "Network acces question" dialog

--- code/InstallPage.cpp
+++ code/InstallPage.cpp
@@ -12,16 +12,25 @@ InstallPage::InstallPage(wxWindow* parent): wxPanel(parent, wxID_ANY) {
 	if (!ProMan::GetProfileManager()->Global()->Read(GBL_CFG_NET_DOWNLOAD_NEWS, &updateNews)) {
 		wxDialog* updateNewsQuestion = new wxDialog(NULL, wxID_ANY, _("Network Access Question"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxSTAY_ON_TOP | wxDIALOG_NO_PARENT);
 		updateNewsQuestion->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW));
-		wxStaticText* updateNewsText = new wxStaticText(updateNewsQuestion, wxID_ANY, _("This launcher can update the news on the front page automaticly.  This update will be done no more than once a day.\n\nThis setting can be changed at anytime on the Install page."));
-		updateNewsText->Wrap(200);
+		wxStaticText* updateNewsText = new wxStaticText(updateNewsQuestion, wxID_ANY, _("This launcher can update the news on the front page automaticly.  This update will be done no more than once a day.\n(This setting can be changed at anytime on the Install page)"), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE);
+		//updateNewsText->Wrap(600);
 		wxButton* allowNewsUpdate = new wxButton(updateNewsQuestion, wxID_ANY, _("Allow launcher to update the front page news"));
 		wxButton* denyNewsUpdate = new wxButton(updateNewsQuestion, wxID_ANY, _("Do not update the news on the front page"));
 		updateNewsQuestion->SetAffirmativeId(allowNewsUpdate->GetId());
 		updateNewsQuestion->SetEscapeId(denyNewsUpdate->GetId());
 		wxBoxSizer* updateNewsSizer = new wxBoxSizer(wxVERTICAL);
+		wxBitmap questionMark(_T("questionmark.png"));
+		wxStaticBitmap* questionImage = new wxStaticBitmap(updateNewsQuestion, wxID_ANY, questionMark);
+		updateNewsSizer->Add(questionImage);
 		updateNewsSizer->Add(updateNewsText);
-		updateNewsSizer->Add(allowNewsUpdate);
-		updateNewsSizer->Add(denyNewsUpdate);
+		wxBoxSizer* choiceSizer = new wxBoxSizer(wxHORIZONTAL);
+		choiceSizer->AddStretchSpacer(1);
+		choiceSizer->Add(allowNewsUpdate);
+		choiceSizer->AddSpacer(5);
+		choiceSizer->Add(denyNewsUpdate);
+		choiceSizer->AddStretchSpacer(1);
+		updateNewsSizer->Add(choiceSizer, wxSizerFlags().Expand().Center());
+		//updateNewsSizer->SetMinSize(wxSize(600,100));
 		updateNewsQuestion->SetSizerAndFit(updateNewsSizer);
 		updateNewsQuestion->Centre(wxBOTH | wxCENTRE_ON_SCREEN);
 
