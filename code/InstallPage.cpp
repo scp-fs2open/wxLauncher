fcd22ba81b603dd951dda088841ae2d76f1f998e
killermosi <killermosi@yahoo.com>
2010-01-18 20:59:02 +0200

    More visual tweaks on the Network Acces Request dialog

--- code/InstallPage.cpp
+++ code/InstallPage.cpp
@@ -10,28 +10,45 @@ InstallPage::InstallPage(wxWindow* parent): wxPanel(parent, wxID_ANY) {
 	updateNewsCheck->SetToolTip(_("Launcher will update the news on next run"));
 	bool updateNews;
 	if (!ProMan::GetProfileManager()->Global()->Read(GBL_CFG_NET_DOWNLOAD_NEWS, &updateNews)) {
-		wxDialog* updateNewsQuestion = new wxDialog(NULL, wxID_ANY, _("Network Access Question"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxSTAY_ON_TOP | wxDIALOG_NO_PARENT);
+		wxDialog* updateNewsQuestion = new wxDialog(NULL, wxID_ANY, _("wxLauncher - Network Access Request"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxSTAY_ON_TOP | wxDIALOG_NO_PARENT);
 		updateNewsQuestion->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW));
-		wxStaticText* updateNewsText = new wxStaticText(updateNewsQuestion, wxID_ANY, _("This launcher can update the news on the front page automaticly.  This update will be done no more than once a day.\n(This setting can be changed at anytime on the Install page)"), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE);
-		//updateNewsText->Wrap(600);
-		wxButton* allowNewsUpdate = new wxButton(updateNewsQuestion, wxID_ANY, _("Allow launcher to update the front page news"));
-		wxButton* denyNewsUpdate = new wxButton(updateNewsQuestion, wxID_ANY, _("Do not update the news on the front page"));
+		wxStaticText* updateNewsText1 = new wxStaticText(updateNewsQuestion, wxID_ANY, _("wxLauncher has the built-in capability of retrieving and displaying the Hard-Light.net highlights on the Welcome page of the launcher."), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE);
+		wxStaticText* updateNewsText2 = new wxStaticText(updateNewsQuestion, wxID_ANY, _("Would you like to allow wxLauncher to go online and retrieve the highlights automatically? "), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE);
+		wxStaticText* updateNewsText3 = new wxStaticText(updateNewsQuestion, wxID_ANY, _("(this setting can can be changed anytime on the Install page, please click \"?\" for more info)"), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE);
+
+		wxButton* allowNewsUpdate = new wxButton(updateNewsQuestion, wxID_ANY, _("Allow automatic Welcome page highlights update"));
+		wxButton* denyNewsUpdate = new wxButton(updateNewsQuestion, wxID_ANY, _("Do not update the highlights on the Welcome page"));
 		updateNewsQuestion->SetAffirmativeId(allowNewsUpdate->GetId());
 		updateNewsQuestion->SetEscapeId(denyNewsUpdate->GetId());
-		wxBoxSizer* updateNewsSizer = new wxBoxSizer(wxVERTICAL);
+		
+		wxBoxSizer* updateNewsSizer = new wxBoxSizer(wxHORIZONTAL);
+		wxBoxSizer* bodySizer= new wxBoxSizer(wxVERTICAL);
+		wxBoxSizer* choiceSizer = new wxBoxSizer(wxHORIZONTAL);
+
 		wxBitmap questionMark(_T("questionmark.png"), wxBITMAP_TYPE_ANY);
 		wxASSERT(questionMark.IsOk());
 		wxStaticBitmap* questionImage = new wxStaticBitmap(updateNewsQuestion, wxID_ANY, questionMark);
-		updateNewsSizer->Add(questionImage);
-		updateNewsSizer->Add(updateNewsText);
-		wxBoxSizer* choiceSizer = new wxBoxSizer(wxHORIZONTAL);
+		
 		choiceSizer->AddStretchSpacer(1);
 		choiceSizer->Add(allowNewsUpdate);
-		choiceSizer->AddSpacer(5);
+		choiceSizer->AddSpacer(15);
 		choiceSizer->Add(denyNewsUpdate);
 		choiceSizer->AddStretchSpacer(1);
-		updateNewsSizer->Add(choiceSizer, wxSizerFlags().Expand().Center());
-		//updateNewsSizer->SetMinSize(wxSize(600,100));
+		
+		bodySizer->AddSpacer(10);
+		bodySizer->Add(updateNewsText1, 0, wxEXPAND | wxCENTER);
+		bodySizer->AddSpacer(5);
+		bodySizer->Add(updateNewsText2, 0, wxEXPAND | wxCENTER);
+		bodySizer->AddSpacer(5);
+		bodySizer->Add(updateNewsText3, 0, wxEXPAND | wxCENTER);
+		bodySizer->AddSpacer(10);
+		bodySizer->Add(choiceSizer, wxSizerFlags().Expand().Center());
+		bodySizer->AddSpacer(10);
+
+		updateNewsSizer->Add(questionImage,0, wxALL | wxCENTER, 5);
+		updateNewsSizer->Add(bodySizer, wxSizerFlags().Expand().Center());
+		updateNewsSizer->AddSpacer(5);
+		
 		updateNewsQuestion->SetSizerAndFit(updateNewsSizer);
 		updateNewsQuestion->Centre(wxBOTH | wxCENTRE_ON_SCREEN);
 
