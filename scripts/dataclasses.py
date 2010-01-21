e586b860a66e04349a0712ca8d5bfcc057a13696
Iss Mneur <iss.mneur@telus.net>
2010-01-21 16:29:40 -0700

    Rearanged onlinehelpmaker.py, split some classes and functions out into their own files.

---
+++ scripts/dataclasses.py
@@ -0,0 +1,5 @@
+class Tag:
+  def __init__(self, name, attrs, data=None):
+    self.name = name
+    self.attributes = attrs
+    self.data = data
--- scripts/onlinehelpmaker.py
+++ scripts/onlinehelpmaker.py
@@ -1,87 +1,16 @@
-import sqlite3, os, sys, tempfile, atexit, traceback, shutil, zipfile, string
+import sqlite3
+import os
+import sys
+import atexit
+import traceback
+import shutil
+import zipfile
+import string
 from optparse import OptionParser
-
 import logging
-NOTICE = 25
-
+NOTICE = 25 # added level for app
 import HTMLParser
 
-class Tag:
-  def __init__(self, name, attrs, data=None):
-    self.name = name
-    self.attributes = attrs
-    self.data = data
-    
-class OutputParser(HTMLParser.HTMLParser):
-  """The class is designed to be used as a base class.  It will output the same html structure as the input file into a file like object (only needs write)."""
-      
-  def __init__(self, file, *args, **kwargs):
-    HTMLParser.HTMLParser.__init__(self, *args, **kwargs)
-    
-    if hasattr(file, 'write'):
-      self.outputfile = file
-    else:
-      raise Exception("file is not a file like object with a write function")
-    
-    self.tagstack = list()
-  
-  def handle_starttag(self, tag, attrs):
-    logging.debug(" Handle starttag: %s", tag)
-    logging.debug("  %s", attrs)
-    self.tagstack.append(Tag(tag, attrs))
-  def handle_startendtag(self, tag, attrs):
-    logging.debug(" Handle start and end tag: %s", tag)
-    logging.debug("  %s", attrs)
-    self.write_tag(Tag(tag, attrs))
-  def handle_endtag(self, tagname):
-    """Finds the matching tag. If the tags are miss matched, function will go down the stack until it finds the match"""
-    logging.debug(" End tag: %s", tagname)
-    tag = self.find_match(tagname)
-    self.write_tag(tag)
-  def handle_data(self, data):
-    logging.debug(" Data: %s", data)
-    if len(self.tagstack) > 0:
-      tag = self.tagstack.pop()
-      if tag.data == None:
-        tag.data = data
-      else:
-        tag.data += data
-      self.tagstack.append(tag)
-    else:
-      self.outputfile.write(data)
-  def find_match(self, tagtofind, indent=0):
-    """Recursive function to match the tag"""
-    tag = self.tagstack.pop()
-    if not tag.name == tagtofind:
-      logging.warning(" %smismatched tag found (expected %d, found %s)", " "*indent, tagtofind, tag.name)
-      self.write_tag(tag)
-      tag = self.find_match(tagtofind)
-      
-    return tag
-  def write_tag(self, tag):
-    """When tag stack is empty, writes tag to file passed in the constructor. When tag stack is not empty formats the tag and sets (or if the next tag.data is not None, appends) the formated string."""
-    if tag.data == None:
-      str = "<%s%s />" %(tag.name, self.format_attributes(tag))
-    else:
-      str = "<%s%s>%s</%s>" %(
-        tag.name, self.format_attributes(tag), tag.data, tag.name)
-        
-    if len(self.tagstack) > 0:
-      tag = self.tagstack.pop()
-      if tag.data == None:
-        tag.data = str
-      else:
-        tag.data += str
-      self.tagstack.append(tag)
-    else:
-      self.outputfile.write(str)
-  def format_attributes(self, tag):
-    """Returns the attributes formatted to be placed in a tag."""
-    ret = ""
-    if len(tag.attributes) > 0:
-      for name, value in tag.attributes:
-        ret = "%s %s=\"%s\"" % (ret, name, value)
-    return ret
 try:
   import markdown
 except ImportError:
@@ -89,6 +18,9 @@ except ImportError:
   print " Make sure that markdown has been installed"
   print "  see the ReadMe.txt for more information"
   raise
+
+from helpparsers import Stage2Parser, Stage3Parser, Stage4Parser, Stage5Parser
+from utilfunctions import change_filename
 
 def main(argv):
   parser = OptionParser(usage="%prog <jobtype> <outfile> <indir> [options]")
@@ -149,11 +81,12 @@ def main(argv):
   logging.debug("Using '%s' as input directory", options.indir )
 
   if options.type == "build":
-    call_logging_exceptions(build, options)
+    ret = call_logging_exceptions(build, options)
   elif options.type == "rebuild":
-    call_logging_exceptions(rebuild, options)
+    ret = call_logging_exceptions(rebuild, options)
   else:
-    call_logging_exceptions(clean, options)
+    ret = call_logging_exceptions(clean, options)
+  sys.exit(ret)
 
 def call_logging_exceptions(func, *args, **kwargs):
   """Protects the caller from all exceptions that occur in the callee. Logs the exceptions that do occur."""
@@ -168,11 +101,16 @@ def call_logging_exceptions(func, *args, **kwargs):
       logging.error("   %s", text)
       
     del tb
+    return 1
+  return 0
     
 def rebuild(options):
   logging.log(NOTICE, "Rebuilding")
-  call_logging_exceptions(clean, options)
-  call_logging_exceptions(build, options)
+  ret = call_logging_exceptions(clean, options)
+  if ret != 0:
+    return ret
+  ret = call_logging_exceptions(build, options)
+  return ret
 
 def clean(options):
   logging.log(NOTICE, "Cleaning..")
@@ -303,7 +241,6 @@ def check_source_newer_than_outfile(options, files):
     logging.warning("Encountered a file (%s) that the os does not like. Forcing build.", "TODO")
     return True
   return False
-    
 
 def generate_input_files_list(options):
   """Returns a list of all input (.help) files that need to be parsed by markdown."""
@@ -319,39 +256,6 @@ def generate_file_list(directory, extension):
         file_list.append(os.path.join(path, file))
   
   return file_list
-  
-def change_filename(filename, newext, orginaldir, destdir, makedirs=True):
-  """Returns the filename after transforming it to be in destdir and making sure the folders required all exist."""
-  filename = os.path.normpath(filename)
-  orginaldir = os.path.normpath(orginaldir)
-  destdir = os.path.normpath(destdir)
-  logging.debug("   change_filename('%s', '%s', '%s', '%s', %s)", filename, newext, orginaldir, destdir, makedirs)
-  outfile_name1 = filename.replace(orginaldir, ".") # files relative name
-  logging.debug("%s", outfile_name1)
-  if newext == None:
-    outfile_name3 = outfile_name1
-  else:
-    outfile_name2 = os.path.splitext(outfile_name1)[0] #file's name without ext
-    logging.debug("%s", outfile_name2)
-    outfile_name3 = outfile_name2 + newext
-  logging.debug("%s", outfile_name3)
-  outfile_name4 = os.path.join(destdir, outfile_name3)
-  logging.debug("%s", outfile_name4)
-  outfile_name = os.path.normpath(outfile_name4)
-  logging.debug("%s", outfile_name)
-  
-  # make sure that the folder exists to output, if wanted
-  if makedirs:
-    outfile_path = os.path.dirname(outfile_name)
-    if os.path.exists(outfile_path):
-      if os.path.isdir(outfile_path):
-        pass # do nothing because the folder already exists
-      else:
-        raise Exception("%s already exists but is not a directory"%(outfile_path))
-    else:
-      os.makedirs(outfile_path)
-    
-  return outfile_name
 
 def process_input_stage1(file, options, files):
   infile = open(file, mode="r")
@@ -375,25 +279,6 @@ def process_input_stage2(file, options, files, helparray):
   outname = change_filename(file, ".stage2", files['stage1'], files['stage2'])
   outfile = open(outname, mode="w")
 
-  class Stage2Parser(OutputParser):
-    def __init__(self, *args, **kwargs):
-      OutputParser.__init__(self, *args, **kwargs)
-      self.control = []
-
-    def handle_starttag(self, tag, attrs):
-      if tag == "meta":
-        if attrs[0][0] == "name" and attrs[0][1] == "control" and attrs[1][0] == "content":
-          self.control.append(attrs[1][1])
-      else:
-        OutputParser.handle_starttag(self, tag, attrs)
-    
-    def handle_startendtag(self, tag, attrs):
-      if tag == "meta":
-        if attrs[0][0] == "name" and attrs[0][1] == "control" and attrs[1][0] == "content":
-          self.control.append(attrs[1][1])
-      else:
-        OutputParser.handle_startendtag(self, tag, attrs)
-
   parser = Stage2Parser(file=outfile)
   parser.feed(input)
   parser.close()
@@ -412,51 +297,6 @@ def process_input_stage3(file, options, files, extrafiles):
   input = infile.read()
   infile.close()
 
-  class Stage3Parser(OutputParser):
-    def __init__(self, options, files, extrafiles, subdir, *args, **kwargs):
-      OutputParser.__init__(self, *args, **kwargs)
-      self.options = options
-      self.files = files
-      self.extrafiles = extrafiles
-      self.subdir = subdir
-      logging.debug(" Subdirectory is %s", subdir)
-
-    def handle_startendtag(self, tag, attrs):
-      """Find the image and copy it to the stage3 folder where it should
-      be in the file output."""
-      if tag == "img":
-        location1 = os.path.join(self.options.indir, self.subdir, attrs[1][1])
-        location = os.path.normpath(location1)
-
-        # check to make sure that the image I am including was in the onlinehelp folder,
-        # if not change the dst name so that it will be located correctly in the stage3 directory
-        if location.startswith(self.options.indir):
-          dst1 = os.path.join(self.files['stage3'], self.subdir, attrs[1][1])
-          dst = os.path.normpath(dst1)
-        else:
-          # get extention
-          basename = os.path.basename(attrs[1][1])
-          (name, ext) = os.path.splitext(basename)
-          (file, outname) = tempfile.mkstemp(ext, name, self.files['stage3'])
-          dst1 = outname.replace(os.getcwd(), ".") # make into a relative path
-
-          # fix up attrs
-          dst = os.path.normpath(dst1)
-          attrs = update_attribute(attrs, 'src', change_filename(dst, os.path.splitext(dst)[1], self.files['stage3'], ".", makedirs=False))
-          
-          
-        logging.debug(" Image (%s) should be in %s and copying to %s", attrs[0][1], location, dst)
-        try:
-          if not os.path.exists(os.path.dirname(dst)):
-            os.mkdir(os.path.dirname(dst))
-          shutil.copy2(location, dst)
-        except:
-          traceback.print_exc()
-          logging.error(" '%s' does not exist", location )
-          sys.exit(3)
-        self.extrafiles.append(dst)
-      OutputParser.handle_startendtag(self, tag, attrs)
-
   outname = change_filename(file, ".stage3", files['stage2'], files['stage3'])
   outfile = open(outname, mode="w")
 
@@ -471,17 +311,6 @@ def process_input_stage3(file, options, files, extrafiles):
   outfile.close()
 
   return outname
-
-def update_attribute(attributes, name, value):
-  """Finds the attribute name and sets it to value in the attributes tuple of tuples, returns the attributes tuple of tuples with the changed attribute."""
-  ret = list()
-  # find name in attributes
-  for n, v in attributes:
-    if n == name:
-      ret.append((name, value))
-    else:
-      ret.append((n,v))
-  return tuple(ret)
   
 def process_input_stage4(file, options, files):
   """Fix the a tags so that they point to the htm files that will be in the output archive."""
@@ -492,25 +321,6 @@ def process_input_stage4(file, options, files):
   outname = change_filename(file, ".stage4", files['stage3'], files['stage4'])
   outfile = open(outname, mode="w")
 
-  class Stage4Parser(OutputParser):
-    def __init__(self, files, *args, **kwargs):
-      OutputParser.__init__(self, *args, **kwargs)
-      self.files = files
-      
-    def handle_starttag(self, tag, attrs):
-      if tag == "a":
-        for name,value in attrs:
-          if name == "href" and not value.startswith("http://"):
-            # make sure the file being refered to exists in stage3
-            check_ref = change_filename(value, ".stage3", ".", files['stage3'], makedirs=False)
-            if not os.path.exists(check_ref):
-              logging.warning(" File (%s) does not exist to be bundled into archive!", check_ref)
-              
-            fixed_ref = change_filename(value, ".htm", ".", ".", makedirs=False)
-            attrs = update_attribute(attrs, "href", fixed_ref)
-      
-      OutputParser.handle_starttag(self, tag, attrs)
-
   parser = Stage4Parser(files=files, file=outfile)
   parser.feed(input)
   parser.close()
@@ -520,22 +330,6 @@ def process_input_stage4(file, options, files):
   
 def process_input_stage5(options, files, extrafiles):
   """Generate the index and table of contents for the output file from the output of stage4."""
-  
-  class Stage5Parser(OutputParser):
-    def __init__(self, *args, **kwargs):
-      OutputParser.__init__(self, *args, **kwargs)
-      self.title = "Untitled"
-    def handle_endtag(self, tag):
-      if tag == "title":
-        t = self.tagstack.pop()
-        self.title = t.data
-        self.tagstack.append(t)
-      elif tag == "h1" and self.title == "Untitled":
-        # make first h1 tag found into the title, will be overwritten by a title tag if it exists.
-        t = self.tagstack.pop()
-        self.title = t.data
-        self.tagstack.append(t)
-      OutputParser.handle_endtag(self, tag)
   
   # write header file
   headerfile_name = os.path.join(files['stage5'], "header.hhp")
