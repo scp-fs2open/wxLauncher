6d3635b55f294cff7cbfa4eab639b9ed38086d9f
Iss Mneur <iss.mneur@telus.net>
2010-01-27 11:30:39 -0700

    BottomButtons are now spaced correctly with UpdatesAviable hidden, and the Fred button removed for now.

--- code/BottomButtons.cpp
+++ code/BottomButtons.cpp
@@ -13,14 +13,17 @@ BottomButtons::BottomButtons(wxWindow* parent, wxPoint &pos, wxSize &size) : wxP
 
 		this->close = new wxButton(this, ID_CLOSE_BUTTON, _("Close"));
 		this->help = new wxButton(this, ID_HELP_BUTTON, _("Help"));
-		this->fred = new wxButton(this, ID_FRED_BUTTON, _("Fred"));
+		this->fred = NULL; // new wxButton(this, ID_FRED_BUTTON, _("Fred"));
 		this->update = new wxButton(this, ID_UPDATE_BUTTON, _("Update Available"));
+		this->update->Hide();
 		this->play = new wxButton(this, ID_PLAY_BUTTON, _("Play"));
 		wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
 		sizer->Add(this->close);
 		sizer->Add(this->help);
-		sizer->Add(this->update);
-		sizer->Add(this->fred);
+		sizer->AddStretchSpacer(1);
+		sizer->Add(this->update, wxSizerFlags().ReserveSpaceEvenIfHidden());
+		sizer->AddStretchSpacer(1);
+		//sizer->Add(this->fred);
 		sizer->Add(this->play);
 
 		this->SetSizer(sizer);
