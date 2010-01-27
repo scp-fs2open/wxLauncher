9cfe9b16ebe9be9b0cf81354cb60aed243d199c8
Iss Mneur <iss.mneur@issmneur.ca>
2010-01-26 21:59:15 -0700

    onlinehelpmaker.py now handles urls that have anchors or only and anchor without mangling it

--- scripts/utilfunctions.py
+++ scripts/utilfunctions.py
@@ -12,35 +12,54 @@ def update_attribute(attributes, name, value):
       ret.append((n,v))
   return tuple(ret)
   
-def change_filename(filename, newext, orginaldir, destdir, makedirs=True):
-  """Returns the filename after transforming it to be in destdir and making sure the folders required all exist."""
-  filename = os.path.normpath(filename)
-  orginaldir = os.path.normpath(orginaldir)
-  destdir = os.path.normpath(destdir)
-  logging.debug("   change_filename('%s', '%s', '%s', '%s', %s)", filename, newext, orginaldir, destdir, makedirs)
-  outfile_name1 = filename.replace(orginaldir, ".") # files relative name
-  logging.debug("%s", outfile_name1)
-  if newext == None:
-    outfile_name3 = outfile_name1
+def split_filename_and_anchor(filename):
+  parts = filename.split("#")
+  if len(parts) > 2:
+    raise Exception("Was passed filename with two (or more) # in it")
+  elif len(parts) == 2:
+    file = parts[0]
+    anchor = parts[1]
   else:
-    outfile_name2 = os.path.splitext(outfile_name1)[0] #file's name without ext
-    logging.debug("%s", outfile_name2)
-    outfile_name3 = outfile_name2 + newext
-  logging.debug("%s", outfile_name3)
-  outfile_name4 = os.path.join(destdir, outfile_name3)
-  logging.debug("%s", outfile_name4)
-  outfile_name = os.path.normpath(outfile_name4)
-  logging.debug("%s", outfile_name)
+    file = parts[0]
+    anchor = None
+  return tuple([file, anchor])
   
-  # make sure that the folder exists to output, if wanted
-  if makedirs:
-    outfile_path = os.path.dirname(outfile_name)
-    if os.path.exists(outfile_path):
-      if os.path.isdir(outfile_path):
-        pass # do nothing because the folder already exists
-      else:
-        raise Exception("%s already exists but is not a directory"%(outfile_path))
+def change_filename(filename, newext, orginaldir, destdir, makedirs=True):
+  """Returns the filename after transforming it to be in destdir and making sure the folders required all exist. Will not damage #anchors at the ends of the paths."""
+  filename = os.path.normpath(filename)
+  filename, anchor = split_filename_and_anchor(filename)
+  if len(filename) != 0:
+    orginaldir = os.path.normpath(orginaldir)
+    destdir = os.path.normpath(destdir)
+    logging.debug("   change_filename('%s', '%s', '%s', '%s', %s)", filename, newext, orginaldir, destdir, makedirs)
+    outfile_name1 = filename.replace(orginaldir, ".") # files relative name
+    logging.debug("%s", outfile_name1)
+    if newext == None:
+      outfile_name3 = outfile_name1
     else:
-      os.makedirs(outfile_path)
+      outfile_name2 = os.path.splitext(outfile_name1)[0] #file's name without ext
+      logging.debug("%s", outfile_name2)
+      outfile_name3 = outfile_name2 + newext
+    logging.debug("%s", outfile_name3)
+    outfile_name4 = os.path.join(destdir, outfile_name3)
+    logging.debug("%s", outfile_name4)
+    outfile_name = os.path.normpath(outfile_name4)
+    logging.debug("%s", outfile_name)
     
-  return outfile_name
+    # make sure that the folder exists to output, if wanted
+    if makedirs:
+      outfile_path = os.path.dirname(outfile_name)
+      if os.path.exists(outfile_path):
+        if os.path.isdir(outfile_path):
+          pass # do nothing because the folder already exists
+        else:
+          raise Exception("%s already exists but is not a directory"%(outfile_path))
+      else:
+        os.makedirs(outfile_path)
+  else:
+    outfile_name = filename
+  
+  if anchor == None:
+    return outfile_name
+  else:
+    return "#".join([outfile_name, anchor])
