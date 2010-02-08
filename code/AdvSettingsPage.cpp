fa17d69e7f70be42bca5643ee96bf64ae48179cf
Iss Mneur <iss.mneur@issmneur.ca>
2010-02-08 11:30:29 -0700

    Reordered AdvSettingsPage taking advantage of the new method on FlagListBox so that error message covers the entire tab.

--- code/AdvSettingsPage.cpp
+++ code/AdvSettingsPage.cpp
@@ -32,81 +32,93 @@ void AdvSettingsPage::OnExeChanged(wxCommandEvent& event) {
 	}
 
 	this->flagListBox = new FlagListBox(this, this->skin);
-	wxHtmlWindow* description = new wxHtmlWindow(this);
-	description->SetPage(_T("<p>Test Page</p>"));
-	
-	wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
-	topSizer->Add(this->flagListBox, wxSizerFlags().Proportion(2).Expand());
-	topSizer->Add(description, wxSizerFlags().Proportion(1).Expand());
-	topSizer->SetMinSize(TAB_AREA_WIDTH, TAB_AREA_HEIGHT/2);
+	if ( this->flagListBox->IsDrawOK() ) {
+		wxHtmlWindow* description = new wxHtmlWindow(this);
+		description->SetPage(_T("<p>Test Page</p>"));
+		
+		wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+		topSizer->Add(this->flagListBox, wxSizerFlags().Proportion(2).Expand());
+		topSizer->Add(description, wxSizerFlags().Proportion(1).Expand());
+		topSizer->SetMinSize(TAB_AREA_WIDTH, TAB_AREA_HEIGHT/2);
 
 
-	wxStaticBitmap* idealIcon = new wxStaticBitmap(this, wxID_ANY, this->skin->GetIdealIcon());
-	wxStaticText* idealLabel = new wxStaticText(this, wxID_ANY, _("= Recommended flag"));
-	wxChoice* flagSetChoice = new wxChoice(this, ID_SELECT_FLAG_SET);
-	wxArrayString flagSetsArray;
-	flagSetChoice->Append(this->flagListBox->GetFlagSets(flagSetsArray));
+		wxStaticBitmap* idealIcon = new wxStaticBitmap(this, wxID_ANY, this->skin->GetIdealIcon());
+		wxStaticText* idealLabel = new wxStaticText(this, wxID_ANY, _("= Recommended flag"));
+		wxChoice* flagSetChoice = new wxChoice(this, ID_SELECT_FLAG_SET);
+		wxArrayString flagSetsArray;
+		flagSetChoice->Append(this->flagListBox->GetFlagSets(flagSetsArray));
 
-	wxBoxSizer* idealFlagsRowSizer = new wxBoxSizer(wxHORIZONTAL);
-	idealFlagsRowSizer->Add(idealIcon);
-	wxBoxSizer* idealLabelSizer = new wxBoxSizer(wxVERTICAL);
-	idealLabelSizer->AddStretchSpacer(1);
-	idealLabelSizer->Add(idealLabel);
-	idealLabelSizer->AddStretchSpacer(1);
-	idealFlagsRowSizer->Add(idealLabelSizer);
-	idealFlagsRowSizer->AddStretchSpacer(1);
-	idealFlagsRowSizer->Add(flagSetChoice, wxSizerFlags().Left());
+		wxBoxSizer* idealFlagsRowSizer = new wxBoxSizer(wxHORIZONTAL);
+		idealFlagsRowSizer->Add(idealIcon);
+		wxBoxSizer* idealLabelSizer = new wxBoxSizer(wxVERTICAL);
+		idealLabelSizer->AddStretchSpacer(1);
+		idealLabelSizer->Add(idealLabel);
+		idealLabelSizer->AddStretchSpacer(1);
+		idealFlagsRowSizer->Add(idealLabelSizer);
+		idealFlagsRowSizer->AddStretchSpacer(1);
+		idealFlagsRowSizer->Add(flagSetChoice, wxSizerFlags().Left());
 
-	wxStaticBox* flagsetNotesBox = new wxStaticBox(this, wxID_ANY, _("Flag set notes"));
-	wxTextCtrl* flagsetNotes = new wxTextCtrl(this, ID_FLAG_SET_NOTES_TEXT,
-		wxEmptyString, wxDefaultPosition, wxSize(-1, TAB_AREA_HEIGHT/8),
-		wxTE_MULTILINE|wxTE_READONLY);
-	wxStaticBoxSizer* flagsetNotesSizer = new wxStaticBoxSizer(flagsetNotesBox, wxVERTICAL);
-	flagsetNotesSizer->Add(flagsetNotes, wxSizerFlags().Expand());
+		wxStaticBox* flagsetNotesBox = new wxStaticBox(this, wxID_ANY, _("Flag set notes"));
+		wxTextCtrl* flagsetNotes = new wxTextCtrl(this, ID_FLAG_SET_NOTES_TEXT,
+			wxEmptyString, wxDefaultPosition, wxSize(-1, TAB_AREA_HEIGHT/8),
+			wxTE_MULTILINE|wxTE_READONLY);
+		wxStaticBoxSizer* flagsetNotesSizer = new wxStaticBoxSizer(flagsetNotesBox, wxVERTICAL);
+		flagsetNotesSizer->Add(flagsetNotes, wxSizerFlags().Expand());
 
-	wxStaticBox* customFlagsBox = new wxStaticBox(this, wxID_ANY, _("Custom flags"));
-	wxTextCtrl* customFlagsText = new wxTextCtrl(this, ID_CUSTOM_FLAGS_TEXT);
-	wxStaticBoxSizer* customFlagsSizer = new wxStaticBoxSizer(customFlagsBox, wxVERTICAL);
-	customFlagsSizer->Add(customFlagsText, wxSizerFlags().Expand());
-	
-	wxStaticBox* commandLineLabel = new wxStaticBox(this, wxID_ANY, _("Current commandline"));
-	wxTextCtrl* commandLineText = new wxTextCtrl(this, ID_COMMAND_LINE_TEXT,
-		wxEmptyString, wxDefaultPosition, wxSize(-1, TAB_AREA_HEIGHT/8),
-		wxTE_MULTILINE|wxTE_READONLY);
-	wxStaticBoxSizer* commandLineSizer = new wxStaticBoxSizer(commandLineLabel, wxVERTICAL);
-	commandLineSizer->Add(commandLineText, wxSizerFlags().Expand());
+		wxStaticBox* customFlagsBox = new wxStaticBox(this, wxID_ANY, _("Custom flags"));
+		wxTextCtrl* customFlagsText = new wxTextCtrl(this, ID_CUSTOM_FLAGS_TEXT);
+		wxStaticBoxSizer* customFlagsSizer = new wxStaticBoxSizer(customFlagsBox, wxVERTICAL);
+		customFlagsSizer->Add(customFlagsText, wxSizerFlags().Expand());
+		
+		wxStaticBox* commandLineLabel = new wxStaticBox(this, wxID_ANY, _("Current commandline"));
+		wxTextCtrl* commandLineText = new wxTextCtrl(this, ID_COMMAND_LINE_TEXT,
+			wxEmptyString, wxDefaultPosition, wxSize(-1, TAB_AREA_HEIGHT/8),
+			wxTE_MULTILINE|wxTE_READONLY);
+		wxStaticBoxSizer* commandLineSizer = new wxStaticBoxSizer(commandLineLabel, wxVERTICAL);
+		commandLineSizer->Add(commandLineText, wxSizerFlags().Expand());
 
-	wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-	sizer->Add(topSizer, wxSizerFlags().Border(wxRIGHT, 25));
-	sizer->Add(idealFlagsRowSizer, wxSizerFlags().Expand().Border(wxRIGHT, 25));
-	sizer->Add(flagsetNotesSizer, wxSizerFlags().Expand().Border(wxRIGHT, 25));
-	sizer->Add(customFlagsSizer, wxSizerFlags().Expand().Border(wxRIGHT, 25));
-	sizer->Add(commandLineSizer, wxSizerFlags().Expand().Border(wxRIGHT, 25));
+		wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+		sizer->Add(topSizer, wxSizerFlags().Border(wxRIGHT, 25));
+		sizer->Add(idealFlagsRowSizer, wxSizerFlags().Expand().Border(wxRIGHT, 25));
+		sizer->Add(flagsetNotesSizer, wxSizerFlags().Expand().Border(wxRIGHT, 25));
+		sizer->Add(customFlagsSizer, wxSizerFlags().Expand().Border(wxRIGHT, 25));
+		sizer->Add(commandLineSizer, wxSizerFlags().Expand().Border(wxRIGHT, 25));
 
-	this->SetSizer(sizer);
-	this->SetMaxSize(wxSize(TAB_AREA_WIDTH, TAB_AREA_HEIGHT));
-	this->Layout();
+		this->SetSizer(sizer);
+		this->SetMaxSize(wxSize(TAB_AREA_WIDTH, TAB_AREA_HEIGHT));
+		this->Layout();
 
-	wxString cmdline, customFlags;
-	ProMan::GetProfileManager()->Get()->
-		Read(PRO_CFG_TC_CURRENT_FLAG_LINE, &cmdline);
-	wxStringTokenizer tokenizer(cmdline, _T(" "));
-	while(tokenizer.HasMoreTokens()) {
-		wxString tok = tokenizer.GetNextToken();
-		if ( this->flagListBox->SetFlag(tok, true) ) {
-			continue;
-		} else {
-			if (!customFlags.IsEmpty()) {
-				customFlags += _T(" ");
+		wxString cmdline, customFlags;
+		ProMan::GetProfileManager()->Get()->
+			Read(PRO_CFG_TC_CURRENT_FLAG_LINE, &cmdline);
+		wxStringTokenizer tokenizer(cmdline, _T(" "));
+		while(tokenizer.HasMoreTokens()) {
+			wxString tok = tokenizer.GetNextToken();
+			if ( this->flagListBox->SetFlag(tok, true) ) {
+				continue;
+			} else {
+				if (!customFlags.IsEmpty()) {
+					customFlags += _T(" ");
+				}
+				customFlags += tok;
 			}
-			customFlags += tok;
 		}
+		customFlagsText->SetLabel(customFlags);
+		this->OnNeedUpdateCommandLine(event);
+	} else {
+		this->flagListBox->SetSize(TAB_AREA_WIDTH, TAB_AREA_HEIGHT);
+		wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+		topSizer->Add(this->flagListBox, wxSizerFlags().Expand());
+		this->SetSizer(topSizer);
+		this->Layout();
 	}
-	customFlagsText->SetLabel(customFlags);
-	this->OnNeedUpdateCommandLine(event);
 }
 
 void AdvSettingsPage::OnNeedUpdateCommandLine(wxCommandEvent &WXUNUSED(event)) {
+	if ( !this->flagListBox->IsDrawOK() ) {
+		// The control I need to update does not exist, do nothing
+		return;
+	}
 	wxTextCtrl* commandLine = dynamic_cast<wxTextCtrl*>(
 		wxWindow::FindWindowById(ID_COMMAND_LINE_TEXT, this));
 	wxCHECK_RET( commandLine != NULL, _T("Unable to find the text control") );
