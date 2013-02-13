21ad9882a1f325073b5f9ba751973c8d1d3e5a14
jg18 <jg18.scp@gmail.com>
2013-02-13 02:03:24 -0600

    fixed VerifyWindowIcon()

--- code/controls/ModList.cpp
+++ code/controls/ModList.cpp
@@ -340,7 +340,6 @@ ModList::ModList(wxWindow *parent, wxSize& size, SkinSystem *skin, wxString tcPa
 
 			readIniFileString(config, _T("/skin/wtitle"), &(item->skin->windowTitle));
 			
-#if 0 // FIXME: remove preprocessing out after tab icons removed
 			wxString *windowIconFile = NULL;
 			readIniFileString(config, _T("/skin/wicon"), &windowIconFile);
 			if ( windowIconFile != NULL ) {
@@ -348,7 +347,6 @@ ModList::ModList(wxWindow *parent, wxSize& size, SkinSystem *skin, wxString tcPa
 					shortname, *windowIconFile);
 				delete windowIconFile;
 			}
-#endif
 
 			readIniFileString(config, _T("/skin/welcometxt"), &(item->skin->welcomePageText));
 
