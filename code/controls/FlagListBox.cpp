1c88e9ce1a50ef3577cdadf0f17a0fcfd08d0401
jg18 <jg18.scp@gmail.com>
2013-04-11 18:20:50 -0500

    FlagListBox::OnDrawItem() on Windows now selects font using the skin system

--- code/controls/FlagListBox.cpp
+++ code/controls/FlagListBox.cpp
@@ -213,9 +213,7 @@ FlagListCheckBoxItem* FlagListBox::FindFlagAt(size_t n) const {
 
 void FlagListBox::OnDrawItem(wxDC &dc, const wxRect &rect, size_t n) const {
 #if IS_WIN32 // replace the ugly default font with one based on the system default
-	// TODO: use SkinSystem::GetFont()?
-	wxFont font(wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT));
-	dc.SetFont(font);
+	dc.SetFont(SkinSystem::GetSkinSystem()->GetFont());
 #endif
 	
 	if (this->IsReady()) {
