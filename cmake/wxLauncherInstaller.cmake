02215b0386fa530f3d0f7204cb1cff2ed07df66c
asarium <asarium@gmail.com>
2016-03-21 11:16:06 +0100

    Add install rules for a desktop file

--- cmake/wxLauncherInstaller.cmake
+++ cmake/wxLauncherInstaller.cmake
@@ -71,7 +71,7 @@ if(IS_APPLE)
 #  set(MACOSX_BUNDLE_LONG_VERSION_STRING "wxLauncher for the SCP, version ${MACOSX_BUNDLE_SHORT_VERSION_STRING}")
   set(MACOSX_BUNDLE_ICON_FILE wxlauncher.icns)
   set(MACOSX_BUNDLE_SHORT_VERSION_STRING ${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH})
-  set(MACOSX_BUNDLE_COPYRIGHT "Copyright © 2009-2015 ${CPACK_PACKAGE_VENDOR}")
+  set(MACOSX_BUNDLE_COPYRIGHT "Copyright Â© 2009-2015 ${CPACK_PACKAGE_VENDOR}")
 endif(IS_APPLE)
 
 if(IS_LINUX)
@@ -144,6 +144,21 @@ elseif(IS_APPLE)
   install(FILES ${PROJECT_SOURCE_DIR}/GPLv2.txt DESTINATION .)
 #  add_custom_target(RemoveAppsLink ALL ${PROJECT_SOURCE_DIR}/platform/macosx/removeAppsLink.sh "${CMAKE_CURRENT_BINARY_DIR}/_CPack_Packages/Darwin/DragNDrop/wxlauncher-${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}" VERBATIM) # attempt to remove link to Applications folder, will keep this as placeholder until I come up with a better idea -- maybe a postinstall script to delete the generated DMG, remove the Applications link, then create a new DMG with hdiutil? 
 else()
-  install(DIRECTORY resources/ DESTINATION ${RESOURCES_PATH})
-  install(FILES ${helphtblocation} DESTINATION ${RESOURCES_PATH})
+  if (${CMAKE_VERSION} VERSION_EQUAL 3.0 OR ${CMAKE_VERSION} VERSION_GREATER 3.0)
+    install(DIRECTORY resources/ DESTINATION ${RESOURCES_PATH})
+    install(FILES ${helphtblocation} DESTINATION ${RESOURCES_PATH})
+
+    # Assume a freedesktop environment
+    set(WXLAUNCHER_FILE ${CMAKE_INSTALL_PREFIX}/bin/$<TARGET_FILE_NAME:wxlauncher>)
+    configure_file("${PROJECT_SOURCE_DIR}/platform/freedesktop/wxLauncher.desktop.in" "${CMAKE_BINARY_DIR}/platform/freedesktop/wxlauncher.desktop")
+
+    file(GENERATE OUTPUT "${CMAKE_BINARY_DIR}/platform/freedesktop/wxlauncher.desktop-$<CONFIG>"
+            INPUT "${CMAKE_BINARY_DIR}/platform/freedesktop/wxlauncher.desktop")
+
+    install(FILES "${CMAKE_BINARY_DIR}/platform/freedesktop/wxlauncher.desktop-$<CONFIG>"
+            DESTINATION share/applications
+            RENAME wxlauncher.desktop)
+  else()
+    message(STATUS "The CMake version is too old to support desktop file generation. CMake 3.0 or newer required")
+  endif()
 endif()
