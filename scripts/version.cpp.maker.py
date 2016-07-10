7e29f9bc7ebfa6c929dc6e8f805d865051c7d033
Iss Mneur <iss.mneur@issmneur.ca>
2016-07-10 09:48:56 -0600

    PEP8ify file

--- scripts/version.cpp.maker.py
+++ scripts/version.cpp.maker.py
@@ -4,38 +4,41 @@ import sys
 
 from version_file_builder import VersionFileBuilder
 
-WORKFILE=3
-OUTFILE=2
-JOB=1
+WORKFILE = 3
+OUTFILE = 2
+JOB = 1
+
 
 def main(argv):
-  parser = OptionParser(usage="%prog <jobtype> <outfile> <workfile> [options]")
-  
-  parser.add_option("", "--gitpath",
-    help="use GITPATH as the executable that will be used to generate the version.cpp file.  Defaults to hg",
-    metavar="GITPATH", default="git")
+    parser = OptionParser(
+        usage="%prog <jobtype> <outfile> <workfile> [options]")
 
-  (options, args) = parser.parse_args(argv)
-  
-  if len(args) != 4:
-    parser.error("Incorrect number of arguments")
-  
-  work = os.path.normcase(os.path.normpath(args[WORKFILE]))
-  file = os.path.normcase(os.path.normpath(args[OUTFILE]))
-  
-  maker = VersionFileBuilder(work, file, options.gitpath)
-  
-  if args[JOB] == "build":
-    maker.build()
-  elif args[JOB] == "rebuild":
-    maker.rebuild()
-  elif args[JOB] == "clean":
-    maker.clean()
-  else:
-    parser.error("Invalid JOB type. Use build, rebuild, or clean.")
-    sys.exit(2)
-  
-  sys.exit(0)
+    parser.add_option("", "--gitpath",
+                      help="use GITPATH as the executable that will be used to generate the version.cpp file.  Defaults to hg",
+                      metavar="GITPATH", default="git")
+
+    (options, args) = parser.parse_args(argv)
+
+    if len(args) != 4:
+        parser.error("Incorrect number of arguments")
+
+    work = os.path.normcase(os.path.normpath(args[WORKFILE]))
+    file = os.path.normcase(os.path.normpath(args[OUTFILE]))
+
+    maker = VersionFileBuilder(work, file, options.gitpath)
+
+    if args[JOB] == "build":
+        maker.build()
+    elif args[JOB] == "rebuild":
+        maker.rebuild()
+    elif args[JOB] == "clean":
+        maker.clean()
+    else:
+        parser.error("Invalid JOB type. Use build, rebuild, or clean.")
+        sys.exit(2)
+
+    sys.exit(0)
+
 
 if __name__ == "__main__":
-  main(sys.argv)
+    main(sys.argv)
