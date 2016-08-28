c7f2a709ce50af5c837b965d4ca187fb5b4938fb
Iss Mneur <iss.mneur@issmneur.ca>
2016-08-28 00:02:15 -0600

    Fix formatting of OutputParser class doc.

--- scripts/ohm/helpparsers.py
+++ scripts/ohm/helpparsers.py
@@ -16,22 +16,24 @@ from .utilfunctions import update_attribute, change_filename
 
 
 class OutputParser(HTMLParser):
-    """The class is designed to be used as a base class.  It will output the same html structure as the input file into a file like object (only needs write)."""
+    """Provide common functionality for the Parsers.
+
+    Currently class passes the input file through to the passed in output"""
 
     def __init__(self, f):
         HTMLParser.__init__(self)
 
         if hasattr(f, 'write'):
-            self.outputfile = f
+            self.output_file = f
         else:
             raise ValueError("f is not a file-like")
 
-        self.tagstack = list()
+        self.tag_stack = list()
 
     def handle_starttag(self, tag, attrs):
         logging.debug(" Handle starttag: %s", tag)
         logging.debug("  %s", attrs)
-        self.tagstack.append(Tag(tag, attrs))
+        self.tag_stack.append(Tag(tag, attrs))
 
     def handle_startendtag(self, tag, attrs):
         logging.debug(" Handle start and end tag: %s", tag)
@@ -46,19 +48,19 @@ class OutputParser(HTMLParser):
 
     def handle_data(self, data):
         logging.debug(" Data: %s", data)
-        if len(self.tagstack) > 0:
-            tag = self.tagstack.pop()
+        if len(self.tag_stack) > 0:
+            tag = self.tag_stack.pop()
             if tag.data == None:
                 tag.data = data
             else:
                 tag.data += data
-            self.tagstack.append(tag)
+            self.tag_stack.append(tag)
         else:
-            self.outputfile.write(data)
+            self.output_file.write(data)
 
     def find_match(self, tagtofind, indent=0):
         """Recursive function to match the tag"""
-        tag = self.tagstack.pop()
+        tag = self.tag_stack.pop()
         if not tag.name == tagtofind:
             logging.warning(" %smismatched tag found (expected %s, found %s)",
                             " " * indent, tagtofind, tag.name)
@@ -75,15 +77,15 @@ class OutputParser(HTMLParser):
             str = "<%s%s>%s</%s>" % (
                 tag.name, self.format_attributes(tag), tag.data, tag.name)
 
-        if len(self.tagstack) > 0:
-            tag = self.tagstack.pop()
+        if len(self.tag_stack) > 0:
+            tag = self.tag_stack.pop()
             if tag.data == None:
                 tag.data = str
             else:
                 tag.data += str
-            self.tagstack.append(tag)
+            self.tag_stack.append(tag)
         else:
-            self.outputfile.write(str)
+            self.output_file.write(str)
 
     def format_attributes(self, tag):
         """Returns the attributes formatted to be placed in a tag."""
@@ -262,12 +264,12 @@ class Stage5Parser(OutputParser):
 
     def handle_endtag(self, tag):
         if tag == "title":
-            t = self.tagstack.pop()
+            t = self.tag_stack.pop()
             self.title = t.data
-            self.tagstack.append(t)
+            self.tag_stack.append(t)
         elif tag == "h1" and self.title == "Untitled":
             # make first h1 tag found into the title, will be overwritten by a title tag if it exists.
-            t = self.tagstack.pop()
+            t = self.tag_stack.pop()
             self.title = t.data
-            self.tagstack.append(t)
+            self.tag_stack.append(t)
         OutputParser.handle_endtag(self, tag)
