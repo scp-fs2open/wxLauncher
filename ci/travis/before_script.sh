3ae50ba53ebadc8916974883f2ad9c508a6d7a7a
Iss Mneur <iss.mneur@issmneur.ca>
2016-09-05 16:14:29 -0600

    Lets make the Travis build matrix be fully useful
    Not sure if this was screwed up by a mis-merge or a copy and paste

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
-set -e
+set -ex
 
 mkdir -p travis-build
 cd travis-build
 
+BUILD_TYPE="-DCMAKE_BUILD_TYPE=$CONFIGURATION"
+
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    $HOME/cmake/bin/cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$CONFIGURATION ..
+    $HOME/cmake/bin/cmake -G "Unix Makefiles" $BUILD_TYPE ..
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    cmake -G "Xcode" -DUSING_SDL_FRAMEWORK=OFF ..
+    cmake -G "Xcode" -DUSING_SDL_FRAMEWORK=OFF $BUILD_TYPE ..
 fi
